b6e140bab93572eed471036b0fbe7861aac6a59c
Antonio <tonito@umich.edu>
2022-02-11 15:29:20 -0500

    Error

--- models/User.js
+++ models/User.js
@@ -4,9 +4,19 @@ const UserSchema = new Schema ({
     userName: {
         type: String,
         required: true,
+        unique: true,
         trim: true
     },
-    email: {},
-    thoughts: {},
-    friends: {},
+    email: {
+        type: String,
+        required: true,
+        unique: true
+        //validation
+    },
+    thoughts: [],
+    friends: [],
 })
+
+const User = model ('User', UserSchema);
+
+module.exports = User;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    deleteUser,
+    updateUser
+} = require('../../controllers/user-controller');
+
+//Get all and POST at api/users
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+//GET one, PUT, and DELETE at api/users/:id
+router('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+module.exports = router;
