db6fb17b996abab0ad11fba23de75556905b04bb
Antonio <tonito@umich.edu>
2022-02-12 08:10:03 -0500

    merge file fixed

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { 
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// api/thoughts
+router.route('/').get(getAllThoughts)
+
+// api/thoughts/(userId)
+router.route('/:userId').post(addThought);
+
+// api/thoughts/(userId)/(thoughtId)
+router
+.route('/:userId/:thoughtId')
+.get(getThoughtById)
+.put(addReaction)
+.delete(removeThought);
+
+router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
+
+module.exports = router;
+
